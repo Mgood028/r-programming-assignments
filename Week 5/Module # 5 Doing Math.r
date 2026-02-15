7ed59c8cf3f703657bbac9208f1b42141bb109cc
Mason Good <masongood01@outlook.com>
2026-02-15 10:58:32 -0500

    week 5

---
+++ Week 5/Module # 5 Doing Math.r
@@ -0,0 +1,22 @@
+# Create matrices without random values
+A <- matrix(1:100, nrow = 10)
+B <- matrix(1:1000, nrow = 10)
+
+# Determinant
+
+det(A)
+det(B)
+
+# Inverse
+solve(A)  
+solve(B)  
+
+# Create a matrix with random values
+C <- matrix(rnorm(100), nrow = 10)  # 10x10
+
+# Determinant
+det(C)
+
+# Inverse
+solve(C)  
+

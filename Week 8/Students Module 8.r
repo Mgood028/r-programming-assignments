1d58ece8a1316e78dc4e0099dbb148e4e43f858a
Mason Good <masongood01@outlook.com>
2026-03-08 11:55:48 -0400

    Week 8 Adds
    Adding more

--- Week 8/Students Module 8.r
+++ Week 8/Students Module 8.r
@@ -1,12 +1,18 @@
 library(plyr)
+
+txt_data <- read.table("Assignment 6 Dataset.txt")
+txt_data
+
 students <- read.table(file.choose(), header = TRUE, sep = ",")
 
 students
 
 gendered_Mean = ddply(students, "Sex", transform, Grade.Average = mean(Grade))
+gendered_Mean
 
 write.table(gendered_Mean , "Students_Gendered_Mean.csv", sep = ",", row.names = FALSE)
 
-i_students <- subset(students, grepl("i", students$Name, ignore.case=T))
+i_students <- subset(students, grepl("i", students$Name, ignore.case=TRUE))
+i_students
 
 write.table(i_students, "Students_With_I.csv", sep = ",", row.names = FALSE)

1aaf81c4537073ec191543b45144cd1ffd46fae9
Mason Good <masongood01@outlook.com>
2026-03-22 14:07:46 -0400

    Week 9
    All week 9 code

---
+++ Week 9/US Gas Market 1950-87.r
@@ -0,0 +1,36 @@
+library(lattice)
+library(ggplot2)
+
+# Load the Data (US Gasoline Market Data (1950-1987, Baltagi))
+gas <- read.csv("USGasG.csv")
+str(gas)
+head(gas)
+
+# Base R Graph
+plot(gas$income, gas$gas,
+     main = "Gas Consumption Vs. Income",
+     xlab = "Income",
+     ylab = "Gas Consumption")
+
+hist(gas$gas,
+     main = "Income",
+     xlab = "Income")
+
+# Lattice
+xyplot(gas ~ income, data = gas,
+       xlab = "Income",
+       ylab = "Gas Consumption", 
+       main = "Gas Consumption vs Income")
+
+# GGplot
+gas$price_category <- cut(gas$price, 
+                          breaks = 3, 
+                          labels = c("Low", "Medium", "High"))
+
+ggplot(gas, aes(x = income, y = gas, color = price_category)) +
+  geom_point(size = 3, alpha = 0.7) +
+  geom_smooth(method = "lm") +
+  labs(title = "Gas Consumption vs Income",
+       x = "Income",
+       y = "Gas Consumption",
+       color = "Gas Price")
